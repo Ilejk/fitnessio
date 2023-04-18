a7656f4647a2c95c7d25ba9d0c919452224cc024
Ilejk <adrianskibaa@gmail.com>
2023-04-18 12:07:08 +0200

    18.04.2023-12:07

--- lib/features/consumption/pages/consumption_page.dart
+++ lib/features/consumption/pages/consumption_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_home_app/features/consumption/providers/consumption_provider.dart';
 import 'package:smart_home_app/features/consumption/widgets/meal_widget.dart';
+import 'package:smart_home_app/utils/managers/color_manager.dart';
+import 'package:smart_home_app/utils/managers/value_manager.dart';
 
 class ConsumptionPage extends StatefulWidget {
   const ConsumptionPage({super.key});
@@ -18,10 +21,13 @@ class _ConsumptionPageState extends State<ConsumptionPage> {
     super.initState();
   }
 
-  @override
-  void didChangeDependencies() {
-    setState(() {});
-    super.didChangeDependencies();
+  Future<void> _handleRefresh() async {
+    setState(() {
+      Provider.of<ConsumptionProvider>(context, listen: false)
+          .fetchAndSetMeals();
+    });
+
+    return await Future.delayed(const Duration(seconds: 2));
   }
 
   @override
@@ -29,31 +35,38 @@ class _ConsumptionPageState extends State<ConsumptionPage> {
     final consumptionProvider =
         Provider.of<ConsumptionProvider>(context, listen: false);
     return SafeArea(
-      child: FutureBuilder<void>(
-          future: consumptionProvider.fetchAndSetMeals(),
-          builder: (context, snapshot) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: consumptionProvider.meals.length,
-                    itemBuilder: (context, index) {
-                      return MealWidget(
-                        title: consumptionProvider.meals[index].title,
-                        amount: consumptionProvider.meals[index].amount,
-                        calories: consumptionProvider.meals[index].calories,
-                        fats: consumptionProvider.meals[index].fats,
-                        carbs: consumptionProvider.meals[index].carbs,
-                        proteins: consumptionProvider.meals[index].proteins,
-                      );
-                    },
+      child: LiquidPullToRefresh(
+        height: SizeManager.s250,
+        color: ColorManager.darkGrey,
+        animSpeedFactor: 2,
+        backgroundColor: ColorManager.white2,
+        onRefresh: _handleRefresh,
+        child: FutureBuilder<void>(
+            future: consumptionProvider.fetchAndSetMeals(),
+            builder: (context, snapshot) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: consumptionProvider.meals.length,
+                      itemBuilder: (context, index) {
+                        return MealWidget(
+                          title: consumptionProvider.meals[index].title,
+                          amount: consumptionProvider.meals[index].amount,
+                          calories: consumptionProvider.meals[index].calories,
+                          fats: consumptionProvider.meals[index].fats,
+                          carbs: consumptionProvider.meals[index].carbs,
+                          proteins: consumptionProvider.meals[index].proteins,
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
-            );
-          }),
+                ],
+              );
+            }),
+      ),
     );
   }
 }
